16017196fe85a49bf7dd186ccbba0ae06f5e4388
enesctnn <enespcetin@gmail.com>
2024-05-06 20:14:39 +0300

    added new project named spotify-profile-app.

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -1,6 +1,9 @@
 import weatherAppGif from '../assets/projects/typeweather/typeweather.gif';
 import weatherAppImg from '../assets/projects/typeweather/typeweather.png';
 
+import spotifyAppGif from '../assets/projects/spotify/spotify.gif';
+import spotifyAppImg from '../assets/projects/spotify/spotify.png';
+
 export const PROJECTS: {
   title: string;
   href: string;
@@ -12,8 +15,8 @@ export const PROJECTS: {
     title: 'TypeWeather',
     href: 'https://typeweather-git-main-enesctnns-projects.vercel.app/',
     description: {
-      en: 'TypeWeather built using Vite, React, and TypeScript, offering users an immersive experience with features like dynamic backgrounds, interactive graphs, and automatic location detection.\n Leveraged modern technologies for efficient state management, responsive design, and seamless navigation.',
-      tr: 'TypeWeather Vite, React ve TypeScript kullanılarak oluşturulan, dinamik arka planlar, etkileşimli grafikler ve otomatik konum algılama gibi özelliklerle kullanıcılara sürükleyici bir deneyim sunuyor.\n Verimli hafıza yönetimi, duyarlı tasarım ve kusursuz gezinme için modern teknolojilerden yararlanıldı.',
+      en: 'Introducing TypeWeather: an innovative weather application crafted with Vite, React, and TypeScript, delivering an immersive weather exploration journey. Engage with dynamic backgrounds, interactive graphs, and seamless automatic location detection for an enriched experience. Powered by modern technologies, TypeWeather ensures efficient state management, responsive design, and effortless navigation, enhancing user interaction and satisfaction.',
+      tr: 'TypeWeather ile tanışın: Vite, React ve TypeScript ile hazırlanmış, sürükleyici bir hava durumu keşif yolculuğu sunan yenilikçi bir hava durumu uygulaması. Zenginleştirilmiş bir deneyim için dinamik arka planlar, etkileşimli grafikler ve kesintisiz otomatik konum algılamayla etkileşime geçin. Modern teknolojilerle desteklenen TypeWeather, etkili durum yönetimi, duyarlı tasarım ve zahmetsiz gezinme sağlayarak kullanıcı etkileşimini ve memnuniyetini artırır.',
     },
     technologies: [
       'Javascript',
@@ -24,11 +27,34 @@ export const PROJECTS: {
       'Recharts',
       'i18next',
       'Framer-Motion',
+      'Tanstack-Query',
     ],
     img: {
       src: weatherAppImg,
-      alt: 'weather app scrolling gif',
+      alt: 'weather app image',
       gif: weatherAppGif,
+    },
+  },
+  {
+    title: 'TypeWeather',
+    href: 'https://github.com/enesctnn/spotify-profile-app',
+    description: {
+      en: 'Spotify Profile App offers deep insights into your music habits, playlist management, and personalized recommendations. Seamlessly connect your Spotify account and explore detailed data, including top artists, tracks, playlists, and recommendations tailored to your taste. Save tracks with timestamped descriptions directly to your playlists for enhanced organization. Discover new music effortlessly. Open in Spotify to delve deeper into track, artist, user, or playlist info.',
+      tr: "Spotify Profil Uygulaması, müzik alışkanlıklarınız, çalma listesi yönetiminiz ve kişiselleştirilmiş önerileriniz hakkında derinlemesine bilgiler sunar. Spotify hesabınızı sorunsuz bir şekilde bağlayın ve en iyi sanatçılar, parçalar, çalma listeleri ve zevkinize göre uyarlanmış öneriler dahil olmak üzere ayrıntılı verileri keşfedin. Gelişmiş organizasyon için parçaları zaman damgalı açıklamalarla doğrudan çalma listelerinize kaydedin. Yeni müziği zahmetsizce keşfedin. Parça, sanatçı, kullanıcı veya çalma listesi bilgilerini daha derinlemesine incelemek için Spotify'da açın.",
+    },
+    technologies: [
+      'Javascript',
+      'Typescript',
+      'React',
+      'React-Router',
+      'Axios',
+      'Recharts',
+      'Tanstack-Query',
+    ],
+    img: {
+      src: spotifyAppImg,
+      alt: 'spotify app',
+      gif: spotifyAppGif,
     },
   },
 ];
--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.Suspense
     fallback={<h1 className="text-center text-4xl">Loading...</h1>}
   >
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </React.Suspense>
 );
